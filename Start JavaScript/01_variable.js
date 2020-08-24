48a916bd1a684a53b7c0745780e6de0a4774b67b
kiteB <69155170+kiteB@users.noreply.github.com>
2020-08-24 17:37:19 +0900

    variable

---
+++ Start JavaScript/01_variable.js
@@ -0,0 +1,13 @@
+// 변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4800;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 4 + mochaPrice * 2);
+console.log(espressoPrice + mochaPrice);
+console.log(espressoPrice + lattePrice * 3 + mochaPrice * 2);
+console.log(lattePrice * 3);
+
+
+
