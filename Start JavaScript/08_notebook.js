ff728d142f50e90a72a2db45e9388521cbc84814
kiteB <69155170+kiteB@users.noreply.github.com>
2020-08-26 21:46:33 +0900

    parameter 응용

---
+++ Start JavaScript/08_notebook.js
@@ -0,0 +1,16 @@
+// 코드를 입력해 주세요.
+function teraToGiga(volume) {
+  console.log(volume + 'TB는');
+  console.log(volume * 1024 + 'GB 입니다.');
+};
+
+function teraToMega(volume) {
+  console.log(volume + 'TB는');
+  console.log(volume * 1024 * 1024 + 'MB 입니다.');
+};
+
+// TB -> GB 테스트
+teraToGiga(2);
+
+// TB -> MB 테스트
+teraToMega(2);
