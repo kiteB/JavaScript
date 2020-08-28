4eded6417ceb9e177b09c9eecaee334fd2c9ff4b
kiteB <69155170+kiteB@users.noreply.github.com>
2020-08-29 00:16:06 +0900

    typeof

---
+++ Core Concept in JavaScript/06_typeof.js
@@ -0,0 +1,6 @@
+// typeof 연산자
+console.log(typeof 1);
+console.log(typeof 1.0);
+console.log(typeof '1');
+console.log(typeof "1");
+console.log(typeof `1`);
---
+++ Core Concept in JavaScript/07_typeof_prac.js
@@ -0,0 +1,4 @@
+console.log(typeof 'Hello' + 'Codeit');
+console.log(typeof 8 - 3);  // NaN (Not a Number) (사칙연산보다 typeof 연산자가 우선순위가 높기 때문!)
+// number가 나오도록 하려면
+console.log(typeof (8 - 3)); 
