0a4359bd7b756a4f8ff28e3eef3cfbb8047e03db
kiteB <69155170+kiteB@users.noreply.github.com>
2020-08-28 21:32:48 +0900

    number, string, boolean

---
+++ Core Concept in JavaScript/01_number.js
@@ -0,0 +1,17 @@
+// 나머지 연산
+console.log(7 % 3);
+
+// 거듭 제곱
+console.log(2 ** 3);
+
+// 덧셈
+console.log(1 + 8);
+
+// 뺄셈
+console.log(6 - 7);
+
+// 곱셈
+console.log(2 * 2);
+
+// 나눗셈
+console.log(5 / 2);
---
+++ Core Concept in JavaScript/02_string.js
@@ -0,0 +1,13 @@
+// 문자열
+console.log('JavaScript');
+console.log("He said \"I'm Iron man\"");
+console.log('He said "I\'m Iron man"');
+console.log("He said \"I\'m Iron man\"");
+console.log('He said \"I\'m Iron man\"');
+
+// ``이용하여 문자열 표현
+console.log(`He said "I'm Iron man"`);
+
+// 문자열 덧셈
+console.log('Hello' + 'Codeit');
+console.log('3' + '5')  // 8이 아님에 주의!
