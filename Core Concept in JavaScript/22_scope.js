8478e08086c00e00853623115eaa16d21392d538
kiteB <69155170+kiteB@users.noreply.github.com>
2020-08-30 17:29:40 +0900

    scope, constant

---
+++ Core Concept in JavaScript/22_scope.js
@@ -0,0 +1,10 @@
+// Scope: 범위, 영역
+let x = 3; // 글로벌 변수, 전역 변수 (Global Variable)
+
+function myFunction() {   // 블록문 (Block Statement)
+  // let x = 3;    // 로컬 변수, 지역 변수 (Local Variable)
+  console.log(x); 
+}
+
+myFunction();
+console.log(x);
