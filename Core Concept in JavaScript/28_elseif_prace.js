c65a6edcc33ad445d9128eae074d5caa757594e9
kiteB <69155170+kiteB@users.noreply.github.com>
2020-08-31 20:50:09 +0900

    else if

---
+++ Core Concept in JavaScript/28_elseif_prace.js
@@ -0,0 +1,21 @@
+function printGrade(midtermScore, finalScore) {
+  let totalScore = midtermScore + finalScore;
+
+  if (totalScore >= 90) {
+      console.log('A');
+  } else if (totalScore >= 80) {
+      console.log('B');
+  } else if (totalScore >= 70) {
+      console.log('C');
+  } else if (totalScore >= 60) {
+      console.log('D');
+  } else {
+      console.log('F');
+  }
+}
+
+// 테스트 코드
+printGrade(25, 35);
+printGrade(50, 45);
+printGrade(29, 24);
+printGrade(37, 42);
