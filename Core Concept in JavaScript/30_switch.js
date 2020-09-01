92d86efc47bd37d7a3ffecdf14cad77c2cacb53d
kiteB <69155170+kiteB@users.noreply.github.com>
2020-09-01 21:39:21 +0900

    switch

---
+++ Core Concept in JavaScript/30_switch.js
@@ -0,0 +1,20 @@
+// switch문 (switch statement)
+
+let myChoice = 5;
+
+switch (myChoice) {
+  case 1:
+    console.log("토끼를 선택한 당신, ...");
+    break;
+  case 2:
+    console.log("고양이를 선택한 당신, ...");
+    break;
+  case 3:
+    console.log("코알라를 선택한 당신, ...");
+    break;
+  case 4:
+    console.log("강아지를 선택한 당신, ...");
+    break;
+  default:
+    console.log("1에서 4 사이의 숫자를 선택해주세요.");
+}
